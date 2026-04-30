9d38d5a0a765a3be2297871d57f7fd7c19c2d175
Osama Mabkhot <osama.zkt@proton.me>
2026-04-30 05:52:22 +0300

    fix(browse): resize data tab right section

--- src/components/structure-tab/SchemaTreePanel.tsx
+++ src/components/structure-tab/SchemaTreePanel.tsx
@@ -2,7 +2,7 @@ import SchemaTree from "./SchemaTree";
 
 export default function SchemaTreePanel() {
   return (
-    <div className="h-full overflow-hidden">
+    <div className="h-full min-w-0 overflow-hidden">
       <div className="h-full overflow-y-auto">
         <SchemaTree />
       </div>
